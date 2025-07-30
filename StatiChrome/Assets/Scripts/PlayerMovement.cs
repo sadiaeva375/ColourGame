2e28f45cec011331e550dd89517be6323622bdd7
Sadia Eva <evasadia375@gmail.com>
2025-07-30 16:32:12 -0300

    Added Player Animations to Tutorial Scene.

--- StatiChrome/Assets/Scripts/PlayerMovement.cs
+++ StatiChrome/Assets/Scripts/PlayerMovement.cs
@@ -9,15 +9,30 @@ public class PlayerMovement : MonoBehaviour
     // variables
     private Vector2 movement;
     private Rigidbody2D rigidBody;
-    [SerializeField] private float speed; 
+    [SerializeField] private float speed;
+    private Animator animator;
 
     private void Awake()
     {
         rigidBody = GameObject.Find("Player").GetComponent<Rigidbody2D>();
+        animator = GameObject.Find("Player").GetComponent<Animator>();
     }
     private void OnMovement(InputValue value)
     {
         movement = value.Get<Vector2>();
+
+        // Player Animation
+        if (movement.x != 0 || movement.y != 0)
+        {
+            animator.SetFloat("X", movement.x);
+            animator.SetFloat("Y", movement.y);
+
+            animator.SetBool("isWalking", true);
+        }
+        else
+        {
+            animator.SetBool("isWalking", false);
+        }
     }
 
     private void FixedUpdate()
